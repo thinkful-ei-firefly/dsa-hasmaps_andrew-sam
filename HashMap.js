df48d7314b462bb275a843818ffb9155ba2a6629
Andrew Jessen-Tyler <andrewtyl@gmail.com>
2019-10-07 10:19:27 -0500

    HashMap.js created

---
+++ HashMap.js
@@ -0,0 +1,19 @@
+class HashMap {
+    constructor(initialCapacity=8) {
+        this.length = 0;
+        this._hashTable = [];
+        this._capacity = initialCapacity;
+        this._deleted = 0;
+    }
+
+    static _hashString(string) {
+        let hash = 5381;
+        for (let i = 0; i < string.length; i++) {
+            hash = (hash << 5) + hash + string.charCodeAt(i);
+            hash = hash & hash;
+        }
+        return hash >>> 0;
+    }
+}
+
+module.exports = HashMap
