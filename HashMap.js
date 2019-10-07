68c68da6ff9eca3f0bbe1bc1a89825cea186e9a6
Andrew Jessen-Tyler <andrewtyl@gmail.com>
2019-10-07 11:02:55 -0500

    1

--- HashMap.js
+++ HashMap.js
@@ -4,21 +4,22 @@ class HashMap {
         this._hashTable = [];
         this._capacity = initialCapacity;
         this._deleted = 0;
+        this.MAX_LOAD_RATIO = 0.5;
+        this.SIZE_RATIO = 3;
     }
 
-    static _hashString(string) {
-        let hash = 5381;
-        for (let i = 0; i < string.length; i++) {
-            hash = (hash << 5) + hash + string.charCodeAt(i);
-            hash = hash & hash;
+    get(key) {
+        const index = this._findSlot(key);
+        if (this._hashTable[index] === undefined) {
+            throw new Error('Key error');
         }
-        return hash >>> 0;
+        return this._hashTable[index].value;
     }
 
     set(key, value){
         const loadRatio = (this.length + this._deleted + 1) / this._capacity;
-        if (loadRatio > HashMap.MAX_LOAD_RATIO) {
-            this._resize(this._capacity * HashMap.SIZE_RATIO);
+        if (loadRatio > this.MAX_LOAD_RATIO) {
+            this._resize(this._capacity * this.SIZE_RATIO);
         }
         const index = this._findSlot(key);
 
@@ -31,6 +32,7 @@ class HashMap {
             DELETED: false
         }; 
     }
+
     delete(key) {
         const index = this._findSlot(key);
         const slot = this._hashTable[index];
@@ -54,17 +56,28 @@ class HashMap {
             }
         }
     }
+
     _resize(size) {
         const oldSlots = this._hashTable;
         this._capacity = size;
         this.length = 0;
+        this._deleted = 0;
         this._hashTable = [];
 
         for (const slot of oldSlots) {
-            if (slot !== undefined) {
+            if (slot !== undefined && !slot.DELETED) {
                 this.set(slot.key, slot.value);
             }
         }
+    }
+
+    static _hashString(string) {
+        let hash = 5381;
+        for (let i = 0; i < string.length; i++) {
+            hash = (hash << 5) + hash + string.charCodeAt(i);
+            hash = hash & hash;
+        }
+        return hash >>> 0;
     }
 }
 
---
+++ HashMaps_drills.js
@@ -0,0 +1,31 @@
+const hashmap = require('./HashMap')
+
+function main() {
+
+    const lor = new hashmap
+    lor.MAX_LOAD_RATIO = 0.5
+    lor.SIZE_RATIO = 3
+
+
+    chars = [{ "Hobbit": "Bilbo" }, { "Hobbit": "Frodo" },
+    { "Wizard": "Gandolf" }, { "Human": "Aragon" }, { "Elf": "Legolas" }, { "Maiar": "The Necromancer" },
+    { "Maiar": "Sauron" }, { "RingBearer": "Gollum" }, { "LadyOfLight": "Galadriel" }, { "HalfElven": "Arwen" },
+    { "Ent": "Treebeard" }]
+
+    for (let i=0; i<chars.length; i++){
+        let thisKey = Object.keys(chars[i])[0]
+        let thisValue = chars[i][thisKey]
+        lor.set(thisKey, thisValue)
+    }
+
+    console.log(lor._hashTable)
+    console.log(lor.get("Maiar"))
+    console.log(lor.get("Hobbit"))
+
+    //"Maiar" is getting overwritten when we fun lor.set("Maiar", "Sauron"). The same is happening for the "Hobbit" key. We are overwriting their values because we are using the same key.
+
+    //Capacity is 24, since the HashTable now has 24 slots, with 9 being filled.
+
+}
+
+main()
